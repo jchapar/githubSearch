631ab74297e940ee76b9489e530f33c56567932b
josechaparro <josechaparro.dev@gmail.com>
2022-03-24 19:43:48 -0700

    search button now gets the search input value

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 "Use Strict";
 
+// Variables
 const toggleLight = document.querySelector(".toggle-light");
 const toggleDark = document.querySelector(".toggle-dark");
 const htmlElement = document.documentElement;
+const searchInput = document.querySelector("#search-input");
+const searchBtn = document.querySelector("#search-btn");
 
+// Theme Toggle
 toggleLight.addEventListener("click", () => {
   htmlElement.setAttribute("data-theme", "light");
   toggleLight.classList.remove("active");
@@ -14,4 +18,10 @@ toggleDark.addEventListener("click", () => {
   htmlElement.setAttribute("data-theme", "dark");
   toggleLight.classList.add("active");
   toggleDark.classList.remove("active");
+});
+
+// Get Search Input
+searchBtn.addEventListener("click", (e) => {
+  console.log(searchInput.value);
+  e.preventDefault();
 });
