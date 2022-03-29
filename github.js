af39a6bc5f9a253d9ce9c9d2a67f7e001e51869b
josechaparro <josechaparro.dev@gmail.com>
2022-03-28 20:45:19 -0700

    hiding JS folders to commit Front end code for challenge

--- github.js
+++ github.js
@@ -18,3 +18,50 @@ class Github {
     };
   }
 }
+
+{
+  /* <script src="github.js"></script>
+<script src="ui.js"></script>
+<script src="app.js"></script> */
+}
+
+//APP JS CODE!!!!!!!
+// // // Init Github
+// const github = new Github();
+// // Init UI
+// const ui = new UI();
+
+// // Load Octocat profile
+// document.addEventListener("DOMContentLoaded", () => {
+//   github.getUser("Octocat").then((data) => {
+//     ui.showProfile(data.profileData);
+//   });
+// });
+
+// // Get Search Input
+// searchBtn.addEventListener("click", (e) => {
+//   console.log(searchInput.value);
+
+//   const userText = searchInput.value;
+
+//   if (userText !== "") {
+//     github.getUser(userText).then((data) => {
+//       if (data.profileData.message === "Not Found") {
+//         errorMsg.style.display = "block";
+//         setTimeout(function () {
+//           errorMsg.style.display = "none";
+//           clearFields();
+//         }, 3000);
+//       } else {
+//         ui.showProfile(data.profileData);
+//         clearFields();
+//       }
+//     });
+//   }
+//   e.preventDefault();
+// });
+
+// // Clear input fields
+// function clearFields() {
+//   searchInput.value = "";
+// }
